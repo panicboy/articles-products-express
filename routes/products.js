1db0af39698a0e001ac9686ac19fa929271a0670
panicboy <asasaki@gmail.com>
2016-07-14 20:07:08 -1000

    added more routing for /products

--- routes/products.js
+++ routes/products.js
@@ -7,31 +7,51 @@ const productsDb = require('../db/products_db');
 app.set('view engine', 'jade');
 app.set('views','../templates');
 
-Router.get('/', (req, res) => {
-  res.send('Hello, I am also here!');
+Router.post('/', (req, res) => {
+  // POST creates a new product
+  var productPostFormat = { name: 'string', price: 'string', inventory: 'number' };
+  var validated = validateParams(req.body, productPostFormat);
+  // add id to new product
+  console.log('body: ', req.body);
+  res.json({success: `${validated}`});
 });
 
-Router.post('/', (req, res) => {
-  var productPostFormat = { name: 'string', price: 'string', inventory: 'number' };
-  console.log('product post: ', validateParams(req.body, productPostFormat));
-  console.log('req.body: ', req.body);
-  res.send('Hello, I am also here!');
+Router.get('/*/edit', (req, res) => {
+    console.log('method: ', req.method, ', body: ', req.body, ', URL: ', req.originalUrl);
+    return res.send('Dude, you totally want to edit a product');
+  });
+
+Router.get('/new', (req, res) => {
+    console.log('method: ', req.method, ', body: ', req.body, ', URL: ', req.originalUrl);
+    res.send('One day you will be able to create a new product here.');
+  });
+
+Router.get('/', (req, res) => {
+  console.log('method: ', req.method, ', body: ', req.body, ', URL: ', req.originalUrl);
+  // responds with HTML generated from your template which displays all Products added thus far.
+      // file name: index.jade
+  console.log('method: ', req.method, ', body: ', req.body, ', URL: ', req.originalUrl);
+  res.send('One day you will be seeing index.jade here.');
+
 });
 
 module.exports = Router;
 
 
-function validateParams(testObject, paramObject, paramVals){
+
+
+function validateParams(theObject, paramSpecs, paramVals){
   var checkParamVals = (arguments.length == 3);
-  var testParams = props(paramObject);
+  var testParams = props(paramSpecs);
   for (var i = 0; i <= testParams.length -1; i++) {
     let param = testParams[i];
-    if(!testObject.hasOwnProperty(param)) return false;
-    if(paramObject[param] == 'boolean' && boolTest(testObject[param])) testObject[param] = boolify(testObject[param]);
-    if(paramObject[param] == 'number' && numTest(testObject[param])) testObject[param] = Number(testObject[param]);
-    // console.log(param,': ', body[param], ', type: ', typeof body[param], ', expected type: ', paramObject[param]);
-    if(typeof testObject[param] != paramObject[param]) return false;
-    if((checkParamVals && paramVals.hasOwnProperty(param)) &&  testObject[param] != paramVals[param]) return false;
+    if(!theObject.hasOwnProperty(param)) return false;
+    let oParam = theObject[param];
+    if(paramSpecs[param] == 'boolean' && boolTest(oParam)) oParam = boolify(oParam);
+    if(paramSpecs[param] == 'number' && numTest(oParam)) oParam = Number(oParam);
+    // console.log(param,': ', body[param], ', type: ', typeof body[param], ', expected type: ', paramSpecs[param]);
+    if(typeof oParam != paramSpecs[param]) return false;
+    if((checkParamVals && paramVals.hasOwnProperty(param)) &&  oParam != paramVals[param]) return false;
   }
   return true;
 }
